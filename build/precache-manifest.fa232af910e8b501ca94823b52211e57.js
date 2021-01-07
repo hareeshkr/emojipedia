7b5f9f530f46f24f824924cf84f5911be3490566
hareeshkr <hareeshkr@pm.me>
2021-01-07 13:07:05 +0530

    build optimised and updated

---
+++ build/precache-manifest.fa232af910e8b501ca94823b52211e57.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d122ff0828fe7b2b43a7fe9c2fc08f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76f01995c5d708dc9299",
+    "url": "/static/js/2.13dec087.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/static/js/2.13dec087.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1ce079fc70ce2811460",
+    "url": "/static/js/main.905b4462.chunk.js"
+  },
+  {
+    "revision": "70967506e22f91c1ea74",
+    "url": "/static/js/runtime-main.b4436dd1.js"
+  }
+]);
